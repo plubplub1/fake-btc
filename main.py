b0c52d45fe6ee46b5134f87b526c2dcd65352273
MIDNIGHT <89833065+plubplub1@users.noreply.github.com>
2022-08-07 19:41:00 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+print("""
+[Status : Error]
+[WORK NAME : Androssy]
+[REGION : TH]
+[CPU : I9 Gen 10th]
+[BPS : 0.000098]
+[EPS : 0.000002]
+[ALL WORKER : 16]
+
+* oof, have bug in a worker
+please contact worker owner!!
+""")
