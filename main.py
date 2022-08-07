f694b4f4d03d86bf8a4140647f015b9f2f6afc26
MIDNIGHT <89833065+plubplub1@users.noreply.github.com>
2022-08-07 19:55:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ print("""
 [Status : Error]
 [WORK NAME : Androssy]
 [REGION : TH]
-[CPU : I9 Gen 10th]
-[BPS : 0.000098]
-[EPS : 0.000002]
+[CPU : i9th Gen 10]
+[BPS : 0.071098] (bitcoin per second)
+[EPS : 0.000002] (energy per second)
 [ALL WORKER : 16]
+[ALL TIME : 13hr]
+
 
 * oof, have bug in a worker
 please contact worker owner!!
